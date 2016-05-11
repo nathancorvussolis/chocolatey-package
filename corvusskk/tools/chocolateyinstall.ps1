67fe0935491008552e6b82f769e2ae29602c021d
nathancorvussolis <nathancorvussolis@gmail.com>
2016-05-11 17:42:09 +0900

    corvusskk v2.3.4

---
+++ corvusskk/tools/chocolateyinstall.ps1
@@ -0,0 +1,42 @@
+﻿$packageName = 'CorvusSKK'
+$installerType = 'exe'
+$url = 'https://github.com/nathancorvussolis/corvusskk/releases/download/2.3.4/corvusskk-2.3.4.exe'
+$silentArgs = "/quiet /norestart"
+$validExitCodes= @(0, 3010)
+
+$osVersion = (Get-WmiObject Win32_OperatingSystem).Version
+
+if(($osVersion -eq [version]"6.0.6000") -or ($osVersion -eq [version]"6.0.6001")) {
+  throw "$packageName needs Service Pack 2 on Windows Vista / Server 2008."
+  return
+}
+
+if($osVersion -eq [version]"6.0.6002") {
+  $hotfix971512 = Get-HotFix | where hotfixID -eq KB971512
+  $hotfix971644 = Get-HotFix | where hotfixID -eq KB971644
+  if(($hotfix971512 -eq $null) -AND ($hotfix971644 -eq $null)) {
+    throw "$packageName needs KB971512 or KB971644 on Windows Vista / Server 2008."
+    return
+  }
+}
+
+if($osVersion -eq [version]"6.1.7600") {
+  throw "$packageName needs Service Pack 1 on Windows 7 / Server 2008 R2."
+  return
+}
+
+if($osVersion -eq [version]"6.3.9600") {
+  $hotfix = Get-HotFix | where hotfixID -eq KB2919355
+  if($hotfix -eq $null) {
+    throw "$packageName needs KB2919355 on Windows 8.1 / Server 2012 R2."
+    return
+  }
+}
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -Url "$url" `
+                          -SilentArgs "$silentArgs" `
+                          -ValidExitCodes $validExitCodes
+
+Write-Warning "Restart Windows for complete installation."
---
+++ corvusskk/tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿$packageName = 'CorvusSKK'
+$installerType = 'exe'
+$bundleUpgradeCode = '{F2664253-EAE9-4ED5-AD92-03229BD8F64F}*'
+$silentArgs = '/uninstall /quiet /norestart'
+$validExitCodes = @(0, 3010)
+
+$keys = Get-ItemProperty -Path @('HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                                 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*') `
+                         -ErrorAction SilentlyContinue `
+        | Where-Object { $_.BundleUpgradeCode -like $bundleUpgradeCode }
+
+foreach($key in $keys) {
+  $file = $key.BundleCachePath
+  Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                              -FileType "$installerType" `
+                              -SilentArgs "$silentArgs" `
+                              -ValidExitCodes $validExitCodes `
+                              -File "$file"
+  Write-Warning "Restart Windows for complete uninstallation."
+  break
+}
