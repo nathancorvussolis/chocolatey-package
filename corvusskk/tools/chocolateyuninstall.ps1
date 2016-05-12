faf71b6eb9a59874f3ecf08d653d89450599fb92
nathancorvussolis <nathancorvussolis@gmail.com>
2016-05-12 21:28:13 +0900

    Fix OS check

--- corvusskk/tools/chocolateyuninstall.ps1
+++ corvusskk/tools/chocolateyuninstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName = 'CorvusSKK'
 $installerType = 'exe'
-$bundleUpgradeCode = '{F2664253-EAE9-4ED5-AD92-03229BD8F64F}*'
+$bundleUpgradeCode = '{F2664253-EAE9-4ED5-AD92-03229BD8F64F}'
 $silentArgs = '/uninstall /quiet /norestart'
 $validExitCodes = @(0, 3010)
 
 $keys = Get-ItemProperty -Path @('HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
                                  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*') `
                          -ErrorAction SilentlyContinue `
-        | Where-Object { $_.BundleUpgradeCode -like $bundleUpgradeCode }
+        | Where-Object { $_.BundleUpgradeCode -contains $bundleUpgradeCode }
 
 foreach($key in $keys) {
   $file = $key.BundleCachePath
@@ -16,6 +16,6 @@ foreach($key in $keys) {
                               -SilentArgs "$silentArgs" `
                               -ValidExitCodes $validExitCodes `
                               -File "$file"
-  Write-Warning "Restart Windows for complete uninstallation."
+  Write-Warning "$packageName requires to restart Windows for complete uninstallation."
   break
 }
