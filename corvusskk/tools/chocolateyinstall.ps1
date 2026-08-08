24717917fcd173b74535fc66d1667ff9bb249a57
nathancorvussolis <nathancorvussolis@gmail.com>
2026-08-09 02:00:50 +0900

    corvusskk-3.3.3

--- corvusskk/tools/chocolateyinstall.ps1
+++ corvusskk/tools/chocolateyinstall.ps1
@@ -1,24 +1,25 @@
 ﻿$packageName = 'CorvusSKK'
 $installerType = 'exe'
-$url = 'https://github.com/nathancorvussolis/corvusskk/releases/download/3.3.2/corvusskk-3.3.2.exe'
-$checksum = 'c2374f934bca2266d38fc7c91dfd3af275b4f8893bd5a04e3b0db1b9d1dbf6ed'
+$url = 'https://github.com/nathancorvussolis/corvusskk/releases/download/3.3.3/corvusskk-3.3.3.exe'
+$checksum = '5F7E277DF4993364704F8737A8A19C41637357BAC825FADEB93BEE81EA638074'
 $checksumType = 'sha256'
-$silentArgs = '/quiet /norestart'
-$validExitCodes= @(0, 3010)
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$validExitCodes = @(0)
 
 $osVersion = [version](Get-WmiObject Win32_OperatingSystem).Version
 
-if($osVersion -lt [version]"10.0.14393") {
+if ($osVersion -lt [version]"10.0.14393") {
   Write-Warning "$packageName supports Windows 10 version 1607 build 14393 or later."
   return
 }
 
-Install-ChocolateyPackage -PackageName "$packageName" `
-                          -FileType "$installerType" `
-                          -Url "$url" `
-                          -Checksum "$checksum" `
-                          -ChecksumType "$checksumType" `
-                          -SilentArgs "$silentArgs" `
-                          -ValidExitCodes $validExitCodes
+Install-ChocolateyPackage `
+  -PackageName "$packageName" `
+  -FileType "$installerType" `
+  -Url "$url" `
+  -Checksum "$checksum" `
+  -ChecksumType "$checksumType" `
+  -SilentArgs "$silentArgs" `
+  -ValidExitCodes $validExitCodes
 
 Write-Warning "$packageName requires to restart Windows for complete installation."
--- corvusskk/tools/chocolateyuninstall.ps1
+++ corvusskk/tools/chocolateyuninstall.ps1
@@ -1,21 +1,26 @@
 ﻿$packageName = 'CorvusSKK'
+$softwareName = 'CorvusSKK*'
 $installerType = 'exe'
-$bundleUpgradeCode = '{F2664253-EAE9-4ED5-AD92-03229BD8F64F}'
-$silentArgs = '/uninstall /quiet /norestart'
-$validExitCodes = @(0, 3010)
+$silentArgs = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART'
+$validExitCodes = @(0)
 
-$keys = Get-ItemProperty -Path @('HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
-                                 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*') `
-                         -ErrorAction SilentlyContinue `
-        | Where-Object { $_.BundleUpgradeCode -contains $bundleUpgradeCode }
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
 
-foreach($key in $keys) {
-  $file = $key.BundleCachePath
-  Uninstall-ChocolateyPackage -PackageName "$packageName" `
-                              -FileType "$installerType" `
-                              -SilentArgs "$silentArgs" `
-                              -ValidExitCodes $validExitCodes `
-                              -File "$file"
+if ($key.Count -eq 1) {
+  $key | % {
+    Uninstall-ChocolateyPackage `
+      -PackageName $packageName `
+      -FileType $installerType `
+      -SilentArgs "$silentArgs" `
+      -ValidExitCodes $validExitCodes `
+      -File "$($_.UninstallString)"
+  }
   Write-Warning "$packageName requires to restart Windows for complete uninstallation."
-  break
+} elseif ($key.Count -eq 0) {
+  Write-Warning "$packageName has already been uninstalled by other means."
+} elseif ($key.Count -gt 1) {
+  Write-Warning "$($key.Count) matches found!"
+  Write-Warning "To prevent accidental data loss, no programs will be uninstalled."
+  Write-Warning "Please alert package maintainer the following keys were matched:"
+  $key | % {Write-Warning "- $($_.DisplayName)"}
 }
